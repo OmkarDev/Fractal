4c22d19f1c31aa55208e3d6c648558c4bd08f2fe
Omkar Dixit <49030608+OmkarDev@users.noreply.github.com>
2019-04-18 15:59:03 +0530

    Added The Sierpinski Carpet

---
+++ Sierpinski Carpet/src/game/Game.java
@@ -0,0 +1,52 @@
+package game;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+import window.Window;
+
+public class Game extends Window implements KeyListener {
+
+	ArrayList<Square> sqaures = new ArrayList<Square>();
+	static int size = 600;
+	int w = size / 7;
+
+	public Game(int width, int height, String title) {
+		super(width, height, title);
+		sqaures.add(new Square(0, 0, w));
+	}
+
+	public void draw() {
+		background(Color.black);
+		g.setColor(Color.white);
+		g.translate(width / 2, height / 2 - 15);
+		for (Square square : sqaures) {
+			g.fillRect((int) square.x, (int) square.y, (int) square.w, (int) square.w);
+		}
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game(Game.size, Game.size, "Sierpinski Carpet");
+		game.fps = 30;
+		game.canvas.addKeyListener(game);
+		game.display();
+	}
+
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	public void keyPressed(KeyEvent e) {
+		ArrayList<Square> newSquares = new ArrayList<Square>();
+		for (Square s : sqaures) {
+			newSquares.add(s);
+			newSquares.addAll(s.makeSquares());
+		}
+		sqaures = newSquares;
+	}
+
+	public void keyReleased(KeyEvent e) {
+	}
+}
